5013edff381d06098fa55b141a7fc1462b4280d8
FraancooP <franco_he_2011_@hotmail.com>
2025-03-28 18:02:58 -0300

    Se agrego modulo de ir y volver con boton

--- PracticoMobile/script.js
+++ PracticoMobile/script.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", function () {
     setTimeout(function () {
         let splash = document.getElementById("splashScreen");
@@ -10,6 +11,50 @@ document.addEventListener("DOMContentLoaded", function () {
         setTimeout(() => {
             splash.style.display = "none"; // Finalmente lo ocultamos
             login.style.display = "flex"; // Aseguramos que se mantenga alineado
-        }, 1000); // Debe coincidir con el tiempo de la transición CSS
+        }, 1); // Debe coincidir con el tiempo de la transición CSS
     }, 5000); // 5 segundos de splash
 });
+
+//ESTO ES UNA IMPLEMENTACION DE WEB PARA EL IR Y VENIR(Preguntar al profe si es eficiente)
+let current_step = 0;
+function stepPreview(){
+    current_step++
+    document.getElementById("barra-progreso").style.display = "block"
+    if(current_step == 1){
+        document.getElementById("progreso-1").style.display = "block"
+        document.getElementById("progreso-2").style.display = "none"
+        document.getElementById("progreso-3").style.display = "none"
+        document.getElementById("transferencia-destino").style.display = "none"
+    }
+    if(current_step == 2){
+        document.getElementById("progreso-1").style.display = "none"
+        document.getElementById("progreso-2").style.display = "block"
+        document.getElementById("progreso-3").style.display = "none"
+    }
+    if(current_step ==3 ){
+        document.getElementById("progreso-1").style.display = "none"
+        document.getElementById("progreso-2").style.display = "none"
+        document.getElementById("progreso-3").style.display = "block"
+    }
+    if(current_step > 3){
+        current_step = 0;
+    }
+}
+function back_Step(){
+    if(current_step == 1){
+        document.getElementById("barra-progreso").style.display = "none"
+        document.getElementById("progreso-1").style.display = "none"
+        document.getElementById("transferencia-destino").style.display = "block"
+        current_step--
+    }
+    if(current_step == 2){
+        document.getElementById("progreso-2").style.display = "none"
+        document.getElementById("progreso-1").style.display = "block"
+        current_step--
+    }
+    if(current_step == 3){
+        document.getElementById("progreso-3").style.display = "none"
+        document.getElementById("progreso-2").style.display = "block"
+        current_step--
+    }
+}
